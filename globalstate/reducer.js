0181f2973a470c0b4b8b15749d0ddff370a2c24e
mnsmanasa <saimanasa1502@gmail.com>
2019-12-30 19:39:21 +0530

    3 files added

---
+++ globalstate/reducer.js
@@ -0,0 +1,22 @@
+import { ADD_TODO, REMOVE_TODO } from './actionTypes';
+
+const initialState = {
+  todos: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todoToAdd: Object.assign({}, action.payload)
+      };
+    case REMOVE_TODO:
+      return {
+        ...state,
+        todoToRemove: Object.assign({}, action.payload)
+      };
+    default:
+      return state;
+  }
+}
